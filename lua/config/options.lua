59dc30294d1307373fb86d17c54543e1721c34c6
Mateus <mateusm.garcia0@gmail.com>
2024-11-02 22:58:19 +0000

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = "<Space>"
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- indent options
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.expandtab = true
