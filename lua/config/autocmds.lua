904a197d1860face956406eeba5b5301b111c189
Mateus <mateusm.garcia0@gmail.com>
2024-11-03 01:18:25 +0000

    Add some plugins and some autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,6 +3,7 @@ local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 autocmd("InsertEnter", {
   pattern = "*",
   callback = function()
+    vim.opt.number = true
     vim.opt.relativenumber = false
   end,
 })
@@ -10,6 +11,16 @@ autocmd("InsertEnter", {
 autocmd("InsertLeave", {
   pattern = "*",
   callback = function()
+    vim.opt.number = false
     vim.opt.relativenumber = true
+  end,
+})
+
+local highlight_yank_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
+autocmd("TextYankPost", {
+  group = highlight_yank_group,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
   end,
 })
