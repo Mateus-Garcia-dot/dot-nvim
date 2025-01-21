bf1186c6757ee792ad89875e416ae210c21359a7
Mateus Medeiros Garcia <mateus.garcia@ebanx.com>
2025-01-21 09:40:52 -0300

    Kill dynamic line toggling
    Got too used to relying on relative line numbers and ended up lost whenever they switched back. Removing the toggling so it's always consistent and doesn’t throw off my navigation.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,21 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
-autocmd("InsertEnter", {
-  pattern = "*",
-  callback = function()
-    vim.opt.number = true
-    vim.opt.relativenumber = false
-  end,
-})
-
-autocmd("InsertLeave", {
-  pattern = "*",
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = true
-  end,
-})
-
 local highlight_yank_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 autocmd("TextYankPost", {
   group = highlight_yank_group,
