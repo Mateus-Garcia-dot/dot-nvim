7624c6ed4417bae160667e8c821a08ee8a9f2246
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2025-01-24 10:43:23 -0300

    Fix dadbod to not fold result queries

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -12,5 +12,12 @@ return {
   },
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
+    -- disable folding
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "dbout",
+      callback = function()
+        vim.opt_local.foldenable = false
+      end,
+    })
   end,
 }
