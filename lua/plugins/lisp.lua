23a185caa19094f25fabeaee7d7939c05272b213
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2026-01-25 09:59:52 -0300

    create editor config and fix indentations

--- lua/plugins/lisp.lua
+++ lua/plugins/lisp.lua
@@ -1,3 +1,3 @@
 return {
-	{ 'vlime/vlime' },
+  { 'vlime/vlime' },
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,11 @@
 local on_highlights = function (h1, c)
-	h1.TelescopeNormal = {
-		fg = c.fg_dark,
-	}
-	h1.TelescopeBorder = {
-		fg = c.bg_dark,
-	}
-	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg="none" })
+  h1.TelescopeNormal = {
+    fg = c.fg_dark,
+  }
+  h1.TelescopeBorder = {
+    fg = c.bg_dark,
+  }
+  vim.api.nvim_set_hl(0, "TelescopeNormal", { bg="none" })
 end
 
 
@@ -13,22 +13,22 @@ return {
   {
     "xiyaowong/transparent.nvim",
     lazy = false,
-		priority = 1000,
+    priority = 1000,
     opts = {
-			extra_groups = {
-				"NormalFloat",
-				"NvimTreeNormal"
-			}
-		},
+      extra_groups = {
+        "NormalFloat",
+        "NvimTreeNormal"
+      }
+    },
   },
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1001,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1001,
     opts = {
-			style = "night",
-			transparent = vim.g.transparent_enabled,
-			on_highlights = on_highlights
-		},
-	}
+      style = "night",
+      transparent = vim.g.transparent_enabled,
+      on_highlights = on_highlights
+    },
+  }
 }
