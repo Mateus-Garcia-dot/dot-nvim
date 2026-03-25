3ca43d9fa0bf99ddd7038a24b087847176deb61e
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2026-03-25 14:32:10 -0300

    Add ctrl-n to oil because its more natural

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<C-n>", "<CMD>Oil<CR>", { desc = "Open Oil file explorer" })
 
 return {
   'stevearc/oil.nvim',
