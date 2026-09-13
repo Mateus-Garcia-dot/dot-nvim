50ffbe3737342f3e424d7afc59357162c079134a
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2026-09-13 18:47:26 -0300

    Remove gitsigns.nvim, keep neogit-only git workflow

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,34 +1,5 @@
 return {
   {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      on_attach = function(bufnr)
-        local gs = require("gitsigns")
-        local function map(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
-        end
-
-        map("n", "]c", function()
-          if vim.wo.diff then return "]c" end
-          vim.schedule(gs.next_hunk)
-          return "<Ignore>"
-        end, "Next hunk")
-        map("n", "[c", function()
-          if vim.wo.diff then return "[c" end
-          vim.schedule(gs.prev_hunk)
-          return "<Ignore>"
-        end, "Prev hunk")
-
-        map({ "n", "v" }, "<leader>gp", gs.preview_hunk, "Preview hunk")
-        -- visual mode resets just the selected lines' hunks, per gitsigns
-        map({ "n", "v" }, "<leader>gr", gs.reset_hunk, "Reset hunk")
-        map({ "n", "v" }, "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame line")
-      end,
-    },
-  },
-  {
-    -- magit-status equivalent
     "NeogitOrg/neogit",
     dependencies = { "nvim-lua/plenary.nvim", "dlyongemallo/diffview.nvim" },
     cmd = "Neogit",
