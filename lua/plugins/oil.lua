5fb2787bb0032785f96f1f19c785d08487befe2e
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2026-09-13 18:47:33 -0300

    Remove global Oil keymaps in favor of the leader-prefixed one
    '-' and <C-n> are dropped; <leader>fj in config/keymaps.lua already opens
    Oil at the current file's directory.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,3 @@
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.keymap.set("n", "<C-n>", "<CMD>Oil<CR>", { desc = "Open Oil file explorer" })
-
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
